ecb01ce6d858cfdbd36db9c2c9aeefbe264433db
ISEN <ISEN@ISENMIC-6846FV5>
2013-11-24 11:59:52 +0100

    roulette done

--- src/casino/vegas/Roulette.java
+++ src/casino/vegas/Roulette.java
@@ -5,15 +5,144 @@
 package casino.vegas;
 
 import static casino.vegas.CasinoVegas.joueur;
+import static casino.vegas.CasinoVegas.croupier;
+
+import java.util.Scanner;
 /**
  *
  * @author isen
  */
 public class Roulette extends Jeux {
     
+    int nombreSorti, gainPossible, nbLance =0;
+    Scanner keyboard = new Scanner(System.in);
+    boolean gagner = false, rejouer = true;
+    
     public void commencer ()
     {
-        
+        System.out.println("Bienvenue dans le jeux de la roulette");
+        règles();
+        while(this.rejouer && joueur.soldeDuCompte > 20)
+        {
+            this.nbLance ++;
+            lancéDuCroupier();
+            miseJoueur();
+            System.out.println("Les jeux sont faits"
+                    + "\n..."
+                    + "\nRien ne va plus");
+            resultat();
+            
+            if(joueur.soldeDuCompte <=20)
+            {
+                System.out.println("Croupier : vous n'avez plus assez d'argent pour continuer à jouer"
+                        + "\nJe vais vous demander de quitter la table");
+                this.rejouer = false;
+            }
+            else
+            {
+                rejouerOuNon();
+            }
+            
+            etatJoueur();
+        }
     }
     
+    
+    public void etatJoueur()
+    {
+        if (this.nbLance == 7)
+            {
+                System.out.println("Vous commencez à devenir accro à ce jeu, Attention !");
+            }
+            else if (this.nbLance > 7 && this.nbLance<13)
+            {
+                System.out.println("Vous êtes accro, tentez de vous arréter au plus vite !!!");
+                joueur.dépendanceJeu --;
+            }
+            else if(this.nbLance >= 13)
+            {
+                System.out.println("Votre état devient réellement critique !"
+                        + "\nVous devez ralentir la cadence ! "
+                        + "\nVous perdez beaucoup d'argent et devenait dangereusement accro");
+                joueur.dépendanceJeu -=3;
+                joueur.etatPsycho --;
+            }
+            
+            croupier.testerJoueur();
+    }
+    
+    
+    public void rejouerOuNon()
+    {
+        String rejouerStr;
+        System.out.println("\n\nVoullez-vous rejouer ?"
+                + "\nTapez 1 pour rejouer");
+        rejouerStr = keyboard.nextLine();
+        
+        if (rejouerStr.equals("1"))
+        {
+            this.rejouer = true;
+        }
+        else
+        {
+            this.rejouer = false;
+        }
+    }
+    
+    public void règles()
+    {
+        System.out.println("Pour jouer : vous devait choisir un ou plusieurs numéros sur lesquels miser et la somme à miser"
+                + "\nLa premiere étape sera le nombre de mise que vous souhaitez faire"
+                + "\nLa deuxième sera le nombre que vous souhaitez miser et combien vous voullez miser");
+    }
+    
+    public void resultat()
+    {
+        if(this.gagner == true)
+            {
+                System.out.println("Croupier : " + this.nombreSorti + " sorti"
+                        + "\nNous avons un gagnant !"
+                        + "\nVous remportez "+ this.gainPossible +"€");
+                joueur.soldeDuCompte += this.gainPossible;
+            }
+            else
+            {
+                System.out.println("Croupier : Rien au numéro");
+            }
+    }
+    
+    
+    public void lancéDuCroupier()
+    {
+        this.nombreSorti = (int) (Math.random()*36);
+    }
+    
+    
+    public void miseJoueur()
+    {
+        int nbDeMise, numeroMise, sommeMise;
+        String nbDeMiseStr, numeroMiseStr, sommeMiseStr;
+        
+        System.out.println("Croupier : Faites vos Jeux");
+        System.out.println("Sur combien de nombre souhaitez vous miser ?");
+        nbDeMiseStr = keyboard.nextLine();
+        nbDeMise = Integer.parseInt(nbDeMiseStr);
+        this.gainPossible = (int) 35/nbDeMise;
+        
+        for(int i =0; i<nbDeMise; i++)
+        {
+            System.out.println("Sur quel chiffre souhaitez-vous miser ?");
+            numeroMiseStr = keyboard.nextLine();
+            numeroMise = Integer.parseInt(numeroMiseStr);
+            System.out.println("Combien souhaitez vous miser dessus ?");
+            sommeMiseStr = keyboard.nextLine();
+            sommeMise = Integer.parseInt(sommeMiseStr);
+            joueur.soldeDuCompte -= sommeMise;
+            
+            if(numeroMise == this.nombreSorti)
+            {
+                this.gagner = true;
+            }
+        }
+    }
 }
